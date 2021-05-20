b72bab7acb95b62888ffb283eb6b3c333a970cad
kotyabuchi <kotyabuchi@gmail.com>
2021-05-20 15:47:03 +0900

    Add: 追加したアイテムをゲーム内登録

--- src/main/kotlin/com/github/kotyabuchi/integratedtech/IntegratedTech.kt
+++ src/main/kotlin/com/github/kotyabuchi/integratedtech/IntegratedTech.kt
@@ -1,6 +1,7 @@
 package com.github.kotyabuchi.integratedtech
 
 import com.github.kotyabuchi.integratedtech.block.ModBlocks
+import com.github.kotyabuchi.integratedtech.item.ModItems
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent
@@ -23,6 +24,7 @@ object IntegratedTech {
 
         // Register the KDeferredRegister to the mod-specific event bus
         ModBlocks.REGISTRY.register(MOD_BUS)
+        ModItems.REGISTRY.register(MOD_BUS)
 
         // usage of the KotlinEventBus
         MOD_BUS.addListener(::onClientSetup)
